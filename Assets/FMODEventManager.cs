f99bc3ce32383f5dc534bafece3f1ec30a6f23c1
Anca Tutescu <anca-stefania.tutescu@haw-hamburg.de>
2020-06-20 13:39:16 +0200

    Untested ButtonDown sound event added for OnTriggerDown

---
+++ Assets/FMODEventManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODEventManager : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string ButtonDownEvent = "";
+
+    private void Start()
+    {
+        InputManager.Instance.RightController.OnTriggerDown += Sound_ButtonDown;
+    }
+
+
+    void OnDisable()
+    {
+        InputManager.Instance.RightController.OnTriggerDown -= Sound_ButtonDown;
+    }
+
+
+    void Sound_ButtonDown()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(ButtonDownEvent);
+    }
+}
